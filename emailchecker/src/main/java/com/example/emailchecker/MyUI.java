4a62755e331a3b1d719bfa38c381cf3b9c8bc59e
AcGordon <al884513@dal.ca>
2017-03-02 00:33:43 -0400

    Question 1
    Added basic email verifier class and method to perform basic email
    verification

--- emailchecker/src/main/java/com/example/emailchecker/MyUI.java
+++ emailchecker/src/main/java/com/example/emailchecker/MyUI.java
@@ -13,7 +13,7 @@ import com.vaadin.ui.UI;
 import com.vaadin.ui.VerticalLayout;
 
 /**
- * This UI is the application entry point. A UI may either represent a browser window 
+ * This UI is  the application entry point. A UI may either represent a browser window 
  * (or tab) or some part of a html page where a Vaadin application is embedded.
  * <p>
  * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
